0225f8b67fc4939c943aebe53c77bf107c43799b
Mehjabin Mostafa <malihamehjabin.mostafa08gmail.com>
2026-02-28 03:02:06 +0600

    First lab

---
+++ lab1.R
@@ -0,0 +1,33 @@
+a<- c(1,2,3,4,5)
+f<- 1:5
+g<- 1.1:5.5
+b<- c(5)
+d<- 8
+e<- c(100,200,300,400,NA)
+a+e
+sum(a+e)
+sum(a+e, na.rm=TRUE)
+
+
+
+char_vec <- c("banana","apple")
+
+num_vec <- c(10,20,30,40,50)
+num_vec[3]=35
+num_vec
+x<- num_vec
+
+
+log_vec <- c(TRUE,FALSE,TRUE,FALSE)
+
+
+num1_vec <- c(2,6,7,4,5)
+num1_vec [c(3,5)]<- c(200,300)
+
+k<- x [x<30]
+
+
+
+
+
+
